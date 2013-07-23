67f969dad5754560ec32bbdb8fc9e8f15263cf58
Zachary Hoefler <zachary.hoefler@gmail.com>
2013-07-23 18:02:53 -0400

    WIP: Quest Repository implementation
    Committing to sync up with Ben's work on the view

--- JustPressPlay/JustPressPlay.UnitTests/TestHelpers/DatabaseHelper.cs
+++ JustPressPlay/JustPressPlay.UnitTests/TestHelpers/DatabaseHelper.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using System.Data.SqlServerCe;
+using System.Diagnostics;
 using System.IO;
 
 namespace JustPressPlay.UnitTests.TestHelpers
@@ -14,11 +16,15 @@ namespace JustPressPlay.UnitTests.TestHelpers
             // Code derived from http://www.codeproject.com/Articles/460175/Two-strategies-for-testing-Entity-Framework-Effort
             AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "")); // see http://stackoverflow.com/questions/12244495/how-to-set-up-localdb-for-unit-tests-in-visual-studio-2012-and-entity-framework/14680912#14680912
             var filePath = Directory.GetCurrentDirectory() + @"\JustPressPlayDB.mdf";
+            var logFilePath = Directory.GetCurrentDirectory() + @"\JustPressPlayDB_log.ldf";
 
             var origConnectionString=@"metadata=res://*/Models.JustPressPlayEF.csdl|res://*/Models.JustPressPlayEF.ssdl|res://*/Models.JustPressPlayEF.msl;provider=System.Data.SqlClient;provider connection string=""data source=(LocalDB)\v11.0;attachdbfilename=|DataDirectory|\JustPressPlayDB.mdf;integrated security=True;MultipleActiveResultSets=True;""";
 
             if (File.Exists(filePath))
                 File.Delete(filePath);
+
+            if (File.Exists(logFilePath))
+                File.Delete(logFilePath);
 
             string connectionString = origConnectionString;//"Datasource = " + filePath;
             Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
@@ -31,6 +37,26 @@ namespace JustPressPlay.UnitTests.TestHelpers
 
             // Connect to DB and return it
             return new JustPressPlayDBEntities(connectionString);
+        }
+
+        public static void TrySaveChanges(ref JustPressPlayDBEntities db)
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+
+                throw dbEx;
+            }
         }
     }
 }
--- JustPressPlay/JustPressPlay/Models/Repositories/QuestRepository.cs
+++ JustPressPlay/JustPressPlay/Models/Repositories/QuestRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 
 using System.Data.Entity;
+using JustPressPlay.ViewModels;
 
 namespace JustPressPlay.Models.Repositories
 {
@@ -19,10 +20,71 @@ namespace JustPressPlay.Models.Repositories
 
 		}
 
+        #region Insert/Delete
+
+        private void AddQuestTemplateToDatabase(quest_template template)
+        {
+            _dbContext.quest_template.Add(template);
+        }
+
+        private void AddAchievementStepsToDatabase(IEnumerable<quest_achievement_step> steps)
+        {
+            foreach (quest_achievement_step step in steps)
+            {
+                _dbContext.quest_achievement_step.Add(step);
+            }
+        }
+
+        public void AdminAddQuest(int id, AddQuestViewModel model)
+        {
+            quest_template newQuest = new quest_template
+            {
+                created_date = DateTime.Now, 
+                creator_id = model.CreatorID, 
+                description = model.Description, 
+                featured = false,
+                icon = model.IconFilePath,
+                last_modified_by_id = null,
+                last_modified_date = null,
+                posted_date = null,
+                retire_date = null,
+                state = 0, // TODO: Get state from enum once it's implemented
+                threshold = model.Threshold,
+                title = model.Title,
+                user_generated = false,
+            };
+
+            List<quest_achievement_step> questAchievementSteps = new List<quest_achievement_step>();
+            foreach (int i in model.SelectedAchievementsList)
+            {
+                quest_achievement_step q = new quest_achievement_step
+                {
+                    achievement_id = i,
+                    quest_id = newQuest.id
+                };
+                questAchievementSteps.Add(q);
+            }
+
+            AddQuestTemplateToDatabase(newQuest);
+            AddAchievementStepsToDatabase(questAchievementSteps);
+
+            Save();
+        }
+
+        #endregion
+
+        #region Query methods
+
         public IEnumerable<quest_tracking> GetTrackedQuestsForUser(int userID)
         {
             return _dbContext.quest_tracking.Where(q => q.user_id == userID);
         }
 
-	}
+        #endregion
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
 }
