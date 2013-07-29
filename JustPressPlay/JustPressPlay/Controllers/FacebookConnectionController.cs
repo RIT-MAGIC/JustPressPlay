4f9308ec34500177aab97b92565a262c446bde42
Zachary Hoefler <zachary.hoefler@gmail.com>
2013-07-29 15:02:30 -0400

    Controller now gets far enough in the Facebook login flow to get a Facebook access token

--- JustPressPlay/JustPressPlay/Controllers/FacebookConnectionController.cs
+++ JustPressPlay/JustPressPlay/Controllers/FacebookConnectionController.cs
@@ -10,6 +10,9 @@ namespace JustPressPlay.Controllers
 {
     public class FacebookConnectionController : Controller
     {
+        string appId = "295662587237899";
+        string appSecret = "2456db6dc3c7c0e8e76913ab6d6e1028"; // TODO: RESET AFTER COMMITTED!
+
         //
         // GET: /FacebookConnection/
 
@@ -27,11 +30,57 @@ namespace JustPressPlay.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult ConnectFacebook(FacebookConnectionViewModel model)
+        public string ConnectFacebook(FacebookConnectionViewModel model)
         {
             var fbClient = new FacebookClient();
+            
 
-            return RedirectToAction("Index");
+            string redirectAfterLoginUri = "http://localhost:5376/FacebookConnection/ProcessFacebookLogin";
+            string fbRedirectUrl = string.Format("https://www.facebook.com/dialog/oauth"
+                                                 + "?client_id={0}"
+                                                 + "&redirect_uri={1}",
+                                                 appId, redirectAfterLoginUri); // TODO: state, response_type: https://developers.facebook.com/docs/facebook-login/login-flow-for-web-no-jssdk/
+            Response.Redirect(fbRedirectUrl);
+
+            // Do we ever get here?
+            return fbClient.Get("zach.hoefler").ToString();
+        }
+
+        [HttpGet]
+        public string ProcessFacebookLogin()
+        {
+            // TODO: process login
+
+            if (Request.QueryString["error"] != null)
+                return "An error occurred :(";
+
+            // Exchange code for an access token
+            string code = Request.QueryString["code"];
+            string redirectAfterLoginUri = "http://localhost:5376/FacebookConnection/ProcessFacebookLogin";
+            object accessTokenGetParams = new
+            {
+                client_id = appId,
+                redirect_uri = redirectAfterLoginUri,
+                client_secret = appSecret,
+                code = code
+            };
+
+            var fbClient = new FacebookClient();
+            JsonObject result = fbClient.Get<JsonObject>("https://graph.facebook.com/oauth/access_token", accessTokenGetParams);
+/*
+    GET https://graph.facebook.com/oauth/access_token?
+    client_id={app-id}
+   &redirect_uri={redirect-uri}
+   &client_secret={app-secret}
+   &code={code-parameter} */
+
+            string accessToken = (string)result["access_token"];
+            Int64 secondsTilExpiration = (Int64)result["expires"];
+            DateTime expireTime = DateTime.Now.AddSeconds(secondsTilExpiration);
+
+            // TODO: inspect token
+
+            return "Expiration DateTime: " + expireTime + ", Seconds til expiration: " + secondsTilExpiration.ToString() + "\n, accessToken: " + accessToken;
         }
     }
 }
