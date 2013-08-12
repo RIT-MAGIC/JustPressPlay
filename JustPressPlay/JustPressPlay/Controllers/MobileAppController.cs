c8e12c76865ae9d054959a8414ebc7c454c45601
Ben Snyder <bws7462@rit.edu>
2013-08-12 12:12:45 -0400

    Hooks For Content Subs, Profile Edits, Log, Mobile
    Committing before getting too far into mobile stuff

---
+++ JustPressPlay/JustPressPlay/Controllers/MobileAppController.cs
@@ -0,0 +1,49 @@
+﻿using JustPressPlay.Models;
+using JustPressPlay.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace JustPressPlay.Controllers
+{
+    public class MobileAppController : Controller
+    {
+        //
+        // GET: /MobileApp/
+
+       /* public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public JsonResult Login(String username, String password)
+        {
+            if (Membership.ValidateUser(username, password))
+            {
+                UnitOfWork work = new UnitOfWork();
+
+                external_token token = work.SystemRepository.GenerateAuthorizationToken(username, Request.UserHostAddress);
+
+                TokenModel tokenModel = new TokenModel()
+                {
+                    Token = token.token,
+                    RefreshToken = token.token
+                };
+
+                return Json(tokenModel, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json("Login Failed", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+
+    public class TokenModel
+    {
+        public String Token { get; set; }
+        public String RefreshToken { get; set; }
+    }
+}
---
+++ JustPressPlay/JustPressPlay/Utilities/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustPressPlay.Utilities
+{
+    public class Logger
+    {
+        #region AchievementInstanceLogging
+        #endregion
+
+        public enum AchievementInstanceLogType
+        {
+            GlobalAssigned,
+            Revoked
+        }
+
+        public enum QuestInstanceLogType
+        {
+            Revoked
+        }
+
+        public enum CommentBehaviorLogType
+        {
+            CommentEdit
+        }
+
+        public enum PlayerEditProfileContentLogType
+        {
+            DisplayNameEdit,
+            FullBioEdit,
+            ProfilePictureEdit,
+            SixWordBioEdit
+        }
+
+        public enum PlayerEditProfileSettingsLogType
+        {
+            Privacy,
+            Facebook,
+            Password,
+            Communications,
+            LeftGame
+        }
+
+        //Action taken by the player involving Friend
+        //(Adding Friends, Ignoring Requests, etc.)
+        public enum PlayerFriendLogType
+        {
+            AddFriend,
+            AcceptRequest,
+            HideRequest,
+            IgnoreRequest,
+            RemoveFriend
+        }
+     
+    }
+}
