064ee1b9dee7d81db67bec6df99ea4a8cf59eaa6
Lance Laughlin <lancealaughlin@gmail.com>
2014-01-24 22:18:22 -0500

    Edit Achievement now dynamically generate the achievment fields based on the number of requirements that already exist for that achievement. Add Achievement has better dynamic form creation.

---
+++ JustPressPlay/JustPressPlay/Content/js/editAchievments.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    for (var i = 1; i < 7; i++) {
+        var currentReq = "#req" + i;
+        var currentField = currentReq + " .small-9 input";
+        currentField = $(currentField).val();
+        if (currentField !== '') {
+            $(currentReq).css("display", "inline");
+            if (i !== 1) {
+                $("#rmv-btn").show();
+            }
+        } else {
+            numReqs = i - 1;
+            break;
+        }
+    }
+});
