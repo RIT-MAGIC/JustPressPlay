cb922b9bb2a240f738ab195f4de92f1d564ce3d4
Lance Laughlin <lancealaughlin@gmail.com>
2014-03-29 15:51:27 -0400

    Added modal functionality for discard draft

--- JustPressPlay/JustPressPlay/Content/js/editAchievments.js
+++ JustPressPlay/JustPressPlay/Content/js/editAchievments.js
@@ -13,4 +13,33 @@
             break;
         }
     }
+
+    if ($("#State").val() == 0) {
+        $("#disregard").show();
+    } else {
+        $("#disregard").hide();
+    }
+
+    buildModal();
 });
+
+function buildModal() {
+    var overlay = jQuery('<div id="modal-background"></div><div id="modal-content"></div>');
+    overlay.appendTo(document.body);
+    jQuery("#modal-content").append("<div id='main-content'><p>Are you sure you want to discard your draft?</p><input type='button' value='Yes' onclick='passID()'/><input type='button' value='No' onclick='hideModal()'/></div>");
+}
+
+
+function checkDiscard() {
+    jQuery("#modal-content, #modal-background").addClass("active");
+}
+
+function hideModal() {
+    jQuery("#modal-content, #modal-background").removeClass("active");
+}
+
+function passID() {
+    var pathName = window.location.pathname.split('/');
+    var id = pathName.pop;
+    console.log(id);
+}
