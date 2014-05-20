7611e3ac6ba3ad9433e92102f38dd2830c40d600
Brandon Littell <brandonl3737@gmail.com>
2014-05-19 20:29:19 -0400

    Setup ajax submissions for friend actions. Still needs more work.

--- JustPressPlay/JustPressPlay/Content/js/jscripts.js
+++ JustPressPlay/JustPressPlay/Content/js/jscripts.js
@@ -1,64 +1,15 @@
-﻿/*
-var $cache = $('.stickyPanel');
-var navHeight = $('.top-bar').height() + 20;
-var distance = $cache.offset().top;
-var scrollThreshold = distance - navHeight;
-
-function fixDiv() {
-    
-
-
-
-    if ($cache.length > 0) {
-        //var distance = $cache.length ? $cache.offset().top : 0,
-        //$window = $(window);
-        
-        //alert(navHeight + "   " + distance + "  " + scrollThreshold);
-        //alert(distance);
-        //var offst = this.outerHeight($('.top-bar'));
-       // var offst = 0;
-
-        if ($(window).scrollTop() >= scrollThreshold ) {
-            $cache.css({ 'position': 'fixed', 'top': navHeight });
-            //$('body').css('padding-top', offst);
-        }
-        else {
-            $cache.css({ 'position': 'relative', 'top': 'auto' });
-            //alert('Shiiiiiiiiiit');
-            //$('body').css('padding-top', '0');
-        }
-    }
-}
-$(window).scroll(fixDiv);
-fixDiv();
-*/
-
-
-
-
-// QR GENERATION
-var create_qrcode = function (text, typeNumber, errorCorrectLevel, table) {
-
-    var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M');
-    qr.addData(text);
-    qr.make();
-
-    //	return qr.createTableTag();
-    return qr.createImgTag();
-};
-
+﻿/* 
+ * jscripts.js
+ * Just Press Play v3
+ */
 
 $(document).ready(function () {
-
 
     /***************************
         SITE-WIDE FEATURES
     ***************************/
 
     FastClick.attach(document.body);
-
-
-    
 
 
     // NAVIGATION DROPDOWNS
@@ -124,6 +75,12 @@ $(document).ready(function () {
         disableFadeOut: false
     });
 
+
+    // Bind click event to friend request buttons (use a generic class selector)
+    // On click call ajax.submit (check function name)
+    // 
+
+    
 
     
 });
